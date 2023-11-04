d1187d8e3a0fab6e7259e2a6c797e46047d96bd9
Elena <elena.lichman@mail.ru>
2023-11-04 23:20:27 +0300

    commit

--- src/main/java/ru/sbgeu/lichman/basic/MainApll.java
+++ src/main/java/ru/sbgeu/lichman/basic/MainApll.java
@@ -4,11 +4,21 @@ import ru.sbgeu.lichman.basic.animals.Cat;
 import ru.sbgeu.lichman.basic.animals.Dog;
 import ru.sbgeu.lichman.basic.animals.Horse;
 
+import java.util.Objects;
+
 public class MainApll {
     public static void main(String[] args) {
-        Cat cat = new Cat("Барсик", 10, 10, 10);
-        Dog dog = new Dog("Оскар", 20, 20, 20);
-        Horse horse = new Horse ("Инфинити", 30, 30, 30);
+        Cat cat = new Cat("Барсик", 50, 300);
+        Dog dog = new Dog("Оскар", 100, 20, 500);
+        Horse horse = new Horse("Инфинити", 150, 30, 1000);
+
+        horse.swim(455);
+        horse.info();
+        cat.info();
+        dog.info();
+        horse.run(300);
+        cat.run(200);
+        dog.run(150);
 
     }
 }
